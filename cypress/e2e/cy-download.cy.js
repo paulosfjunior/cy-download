970bb4bdfbe89b5392f96465dc8875e646151025
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2023-03-14 01:14:09 -0300

    Revisado função de teste

--- cypress/e2e/cy-download.cy.js
+++ cypress/e2e/cy-download.cy.js
@@ -1,29 +1,35 @@
-describe('verify download functionality', () => {
+describe('verify download functionality and clear funcionality', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('downloads small file', () => {
+    cy.clearDownload();
+
     cy.get('[data-cy="small"]').click();
 
     cy.verifyDownload('small-file-10MB.zip');
   });
 
   it('downloads large file', () => {
+    cy.clearDownload();
+
     cy.get('[data-cy="large"]').click();
 
     cy.verifyDownload('large-file-100MB.zip', { timeout: 55000, interval: 500 });
   });
 
   it('downloads with contains option', () => {
-    cy.exec(`rm -rf ${Cypress.config('downloadsFolder')}`);
+    cy.clearDownload();
+
     cy.get('[data-cy="large"]').click();
 
     cy.verifyDownload('large-file-100MB', { contains: true });
   });
 
   it('downloads file with extension and contains option', () => {
-    cy.exec(`rm -rf ${Cypress.config('downloadsFolder')}`);
+    cy.clearDownload();
+    
     cy.get('[data-cy="large"]').click();
 
     cy.verifyDownload('large-file-100MB.zip', { contains: true });
